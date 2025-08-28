7fe15d8fa84bc5f07fded6e972c5347becb59a57
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-08-28 02:28:35 +0200

    Add Player Prototype new height styles for player prototype and update portfolio page
    - Introduced new height styles for the player prototype in _animation-components.scss to ensure full viewport height
    - Updated portfolio.vue to include the ProjectPlayerPrototype component for enhanced project display
    - Cleaned up commented-out components for better readability and structure

--- app/pages/portfolio.vue
+++ app/pages/portfolio.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
     <h1 class="sr-only">Portfolio</h1>
-    <!-- <OppositeDirectionMarquees />
-    <SimultaneousWords
-      theme="light"
-      paragraph="David Hockney (1937) is a British artist renowned for his vibrant use of color and innovative exploration of perspective. He is best known for his paintings of California swimming pools which capture light and atmosphere with striking simplicity."
-      :pin-height-vh="500"
-    /> -->
+    <ProjectPlayerPrototype :showDevTools="true" />
     <ProjectFortPrivacy :show-dev-tools="true" />
     <ProjectSliwka :show-dev-tools="true" />
     <ProjectVibeuu :show-dev-tools="true" />
-    <!-- <ProjectZaksa :show-dev-tools="true" />
+    <ProjectZaksa :show-dev-tools="true" />
     <ProjectInfOrca :show-dev-tools="true" />
     <ProjectWorkingJobsVyne :show-dev-tools="true" />
     <ProjectSpectrum :show-dev-tools="true" />
     <ProjectArtTech :show-dev-tools="true" />
-    <ProjectBlueberry :show-dev-tools="true" /> -->
   </div>
 </template>
 
