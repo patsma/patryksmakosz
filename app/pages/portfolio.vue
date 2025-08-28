a4633d78e1ca81d240f097cc9e78a852e16fb580
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-08-28 22:32:14 +0200

    Add Page404 animation styles and update portfolio page
    - Introduced new animation styles for the Page404 component in _animation-components.scss to ensure proper height adjustments
    - Updated portfolio.vue to temporarily render the ProjectPage404 component for testing the new animation
    - Commented out other project components for cleaner structure during testing

--- app/pages/portfolio.vue
+++ app/pages/portfolio.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="sr-only">Portfolio</h1>
-    <ProjectPlayerPrototype :showDevTools="true" />
+    <!-- <ProjectPlayerPrototype :showDevTools="true" />
     <ProjectFortPrivacy :show-dev-tools="true" />
     <ProjectSliwka :show-dev-tools="true" />
     <ProjectVibeuu :show-dev-tools="true" />
@@ -9,7 +9,10 @@
     <ProjectInfOrca :show-dev-tools="true" />
     <ProjectWorkingJobsVyne :show-dev-tools="true" />
     <ProjectSpectrum :show-dev-tools="true" />
-    <ProjectArtTech :show-dev-tools="true" />
+    <ProjectArtTech :show-dev-tools="true" /> -->
+
+    <!-- Temporary render for testing Page404 animation -->
+    <ProjectPage404 :show-dev-tools="true" />
   </div>
 </template>
 
